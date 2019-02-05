a095a9692e5fb721812ef80ee31a1fe8eefb2b53
Ernest Rudnicki <terminat12@gmail.com>
2019-02-05 11:20:02 +0100

    feat: requirejs library added

--- app/main.js
+++
@@ -1,4 +0,0 @@
-/*
-let element = document.querySelector(".gameContainer__mainMenu");
-element.classList.add("gameContainer__mainMenu--fade");
-*/
---
+++ app/scripts/main.js
@@ -0,0 +1,1 @@
+requirejs(["./scripts/game.js"], function(util) {});
