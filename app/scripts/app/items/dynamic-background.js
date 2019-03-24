0bab680ca75da9502c9c801f43daa36cb118b268
Ernest Rudnicki <terminat12@gmail.com>
2019-03-24 15:59:48 +0100

    dynamic background functionality added

--- app/scripts/app/items/dynamic-background.js
+++ app/scripts/app/items/dynamic-background.js
@@ -1,40 +1,54 @@
 define(() => {
   return {
     intervalId: null,
-    turn: "front",
-    imgQuantity: 8,
+    imgQuantity: 6,
+    turn: "right",
+    lastIndex: null,
 
-    dynamicallyChangeBackground: function () {
+    dynamicallyChangeBackground: function() {
       const backgroundFront = document.querySelector(".backgroundFront"),
-        backgroundBack = document.querySelector(".backgroundRear");
+        backgroundRear = document.querySelector(".backgroundRear");
+
       this.intervalId = setInterval(() => {
-        this.changeImage(backgroundFront, backgroundBack);
-        console.log("changing");
+        this.changeImage(backgroundRear, backgroundFront);
 
-        if (backgroundFront.classList.contains("anDisappear")) {
-          backgroundFront.classList.remove("anDisappear");
-          backgroundBack.classList.add("anDisappear");
-        } else if (backgroundBack.classList.contains("anDisappear")) {
-          backgroundBack.classList.remove("anDisappear");
-          backgroundFront.classList.add("anDisappear");
+        if (this.turn === "right") {
+          backgroundFront.classList.add("anComeFromRight");
+          backgroundFront.classList.remove("anComeFromLeft");
+          this.turn = "left";
+        } else if (this.turn === "left") {
+          backgroundFront.classList.add("anComeFromLeft");
+          backgroundFront.classList.remove("anComeFromRight");
+          this.turn = "right";
         }
-      }, 2000)
-
+      }, 20000);
     },
-    changeImage: function (backgroundBack, backgroundFront) {
-      if (this.turn === "front") {
-        backgroundBack.style.backgroundImage = `url('${this.randomlyPickImg}')`;
-        turn = "back";
-      } else if (this.turn === "back") {
-        backgroundFront.style.backgroundImage = `url('${this.randomlyPickImg}')`;
-        turn = "front";
+    changeImage: function(backgroundRear, backgroundFront) {
+      if (this.lastIndex !== null) {
+        backgroundRear.style.backgroundImage = `url('../img/background/background_0${
+          this.lastIndex
+        }.jpg')`;
+      } else {
+        backgroundRear.style.backgroundImage =
+          "url(../img/background/backgroundMain_01.jpg)";
       }
+      backgroundFront.style.backgroundImage = `url('${this.randomlyPickImg()}')`;
     },
-    randomlyPickImg: function () {
-      return `../img/background/background_0${Math.floor(Math.random * this.imgQuantity)}`
+    randomlyPickImg: function() {
+      let index = Math.floor(Math.random() * this.imgQuantity);
+      while (this.lastIndex === index) {
+        index = Math.floor(Math.random() * this.imgQuantity);
+      }
+      this.lastIndex = index;
+      return `../img/background/background_0${index}.jpg`;
     },
-    setMainMenuBackground: function () {
-      document.querySelector(".backgroundFront").style.backgroundImage = "url(../img/background/backgroundMain_01.jpg)";
+    setMainMenuBackground: function() {
+      document.querySelector(".backgroundFront").style.backgroundImage =
+        "url(../img/background/backgroundMain_01.jpg)";
+    },
+    stopBackgroundAnimations: function() {
+      this.setMainMenuBackground();
+      clearInterval(this.intervalId);
     }
-  }
-})
+  };
+});
