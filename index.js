0b31c9af0d63440c0f3d5e8f48fa6e24e95df600
Ernest <terminat12@gmail.com>
2019-05-12 15:43:43 +0200

    express.js framework added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "app")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "app", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server is up and running");
+});
