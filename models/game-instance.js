13b22e8a1c7b50ec9d451116ab61ae29431d84c2
Ernest Rudnicki <terminat12@gmail.com>
2019-05-13 20:31:20 +0200

    Game Instance model filled

--- models/game-instance.js
+++ models/game-instance.js
@@ -1,1 +1,20 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+
+var GameInstanceSchema = new mongoose.Schema({
+  dateTime: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
+    unique: false
+  },
+  token: {
+    type: String,
+    required: true
+  }
+});
+
+var GameInstance = mongoose.model("User", GameInstance);
+module.exports = { GameInstance };
